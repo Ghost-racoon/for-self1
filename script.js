1a5cabf124273b0fe934cd7866d5baada662f592
Ghost-racoon <bharathsahadeva2005@gmail.com>
2025-08-05 11:02:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// ===== Smooth Scroll to Sections =====
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
